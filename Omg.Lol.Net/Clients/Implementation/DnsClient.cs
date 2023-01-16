0c263b1ccdcf27c933915fdd1e4d7dd5afe2c81a
Wayne Yao <wx.yao@outlook.com>
2023-01-17 00:49:07 +0800

    Stash push

---
+++ Omg.Lol.Net/Clients/Implementation/DnsClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Omg.Lol.Net.Clients.Implementation;
+
+using Omg.Lol.Net.Clients.Abstract;
+
+public class DnsClient : IDnsClient
+{
+    public string Token { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+}
---
+++ Omg.Lol.Net/Models/DnsRecordDetail.cs
@@ -0,0 +1,31 @@
+﻿namespace Omg.Lol.Net.Models;
+
+using System;
+using Newtonsoft.Json;
+
+public class DnsRecordDetail
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = string.Empty;
+
+    [JsonProperty("type")]
+    public string Type { get; set; } = string.Empty;
+
+    [JsonProperty("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonProperty("data")]
+    public string Data { get; set; } = string.Empty;
+
+    [JsonProperty("priority")]
+    public int? Priority { get; set; }
+
+    [JsonProperty("ttl")]
+    public int Ttl { get; set; }
+
+    [JsonProperty("created_at")]
+    public DateTimeOffset CreatedAt { get; set; }
+
+    [JsonProperty("updated_at")]
+    public DateTimeOffset UpdatedAt { get; set; }
+}

22fe806d9ec310b11fba9d89a2f3ccf1bef7f73d
Wayne Yao <wx.yao@outlook.com>
2023-02-21 20:29:43 +0800

    Add Directory client

---
+++ Omg.Lol.Net/Clients/Implementation/DirectoryClient.cs
@@ -0,0 +1,29 @@
+﻿namespace Omg.Lol.Net.Clients.Implementation;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Omg.Lol.Net.Clients.Abstract;
+using Omg.Lol.Net.Infrastructure;
+using Omg.Lol.Net.Models;
+using Omg.Lol.Net.Models.Directory;
+
+public class DirectoryClient : IDirectoryClient
+{
+    public string Token { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    private const string RetrieveDirectoryAddressEndpoint = "/directory";
+
+    private readonly IApiServerCommunicationHandler apiServerCommunicationHandler;
+
+    public DirectoryClient(IApiServerCommunicationHandler apiServerCommunicationHandler) =>
+        this.apiServerCommunicationHandler = apiServerCommunicationHandler;
+
+    public async Task<CommonResponse<AddressDirectory>> GetAddressDirectoryAsync(
+        CancellationToken cancellationToken = default)
+        => await this.apiServerCommunicationHandler.GetAsync<CommonResponse<AddressDirectory>>(
+                this.Url + RetrieveDirectoryAddressEndpoint,
+                cancellationToken)
+            .ConfigureAwait(false);
+}
---
+++ Omg.Lol.Net/Models/Directory/AddressDirectory.cs
@@ -0,0 +1,12 @@
+﻿namespace Omg.Lol.Net.Models.Directory;
+
+using System;
+
+public class AddressDirectory
+{
+    public string Message { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    public string[] Directory { get; set; } = Array.Empty<string>();
+}
